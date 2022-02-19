121d953a5ae80299954f1ee8f4b5f88e46876e5e
12moi <moi.shadrack@student.moringaschool.com>
2022-02-19 08:51:38 +0300

    adding @classmethod to user class

--- user.py
+++ user.py
@@ -1,4 +1,7 @@
 
+from collections import UserList
+
+
 class User:
     '''
     class that generates a new user instance
@@ -13,4 +16,14 @@ class User:
         save_user method saves a new user objects to the user_list
         '''
         User.user_list.append(self)
+    
+    @classmethod
+    def diplay_user(cls):
+        return cls.user_list
+    def delete_user(self):
+         '''
+         A method that deletes a saved account from the list
+         '''
+         UserList.user_list.remove(self)
+        
         
